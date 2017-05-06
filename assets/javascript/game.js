ecd70b7e11d22bce933a1751278fd3ba9e473f1a
JMJ <jmj@JMJs-MacBook-Pro.local>
2017-05-06 09:01:06 -0700

    Final Release

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,114 +1,114 @@
-$(document).ready(function(){
 
-//Variables================================================================
-var goal
-var gem1
-var gem2
-var gem3
-var gem4
-var score
-var wins=0;
-var losses=0;
+//sets up random number user is trying to match
+	$( document ).ready(function(){
+		var Random=Math.floor(Math.random()*101+19)
+		console.log(Random)
+		$('#number').text(Random);
 
-//Set-up gems===============================================================
-function setUp(){
-	goal = Math.floor(Math.random()*120+19);
-	gem1 = Math.floor(Math.random()*12+1);
-	gem2 = Math.floor(Math.random()*12+1);
-	gem3 = Math.floor(Math.random()*12+1);
-	gem4 = Math.floor(Math.random()*12+1);
-	score=0;
-	$('#goal').html(goal)
-	console.log("Gem1: "+gem1);
-	console.log("Gem2: "+gem2);
-	console.log("Gem3: "+gem3);
-	console.log("Gem4: "+gem4);
-};
-//Sounds========================================
-var click=document.createElement("audio");
-click.src="assets/Sounds/ComputerBoop.wav";
+//	setting up four random numbers array, total variable and printing the wins & losses variables
+	var GeneratedNumbers = []
+	var total= 0;
+	var wins= 0;
+	var losses = 0;
+	$('#numberWins').text(wins);
+	$('#numberLosses').text(losses);
 
-var winner=document.createElement("audio");
-winner.src="assets/Sounds/robotWinner.wav"
 
-var loser=document.createElement("audio");
-loser.src="assets/Sounds/robotLoser.wav"
-
-//Check for outcome of the click=============================================
-	function outcome(){
-		if(score===goal){
-			$('.outcome').html("Winner is you");
-			winner.play();
-			wins++;
-			$('#score').empty();
-			setUp();
-		}
-		else if(score>goal){
-			$('.outcome').html("loser is you");
-			loser.play();
-			losses++;
-			$('#score').empty();
-			setUp();
-		}
-
-		$('#wins').html("Wins: "+wins);
-		$('#losses').html("Losses: "+losses)
+//sets up random numbers for each jewel
+function fourRandomNumbers(){
+	for (var i = 0; i < 4; i++) {
+		var num = Math.floor(Math.random()*11+1);
+		GeneratedNumbers.push(num);
 	}
+	console.log(GeneratedNumbers)
+}
 
-//adding bounces============================================================
-$.fn.extend({
-	animateCss: function (animationName){
-		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-		$(this).addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName);
-        });
-    }
-})
+fourRandomNumbers();
 
-//GamePlay===================================================================
-function playGame(){
-		$('#gem1').on("click", function(){
-			click.play();
-			$('#gem1').animateCss('tada');
-			score+=gem1;
-			$('#score').html(score);
-			outcome();
+
+//resets the game
+function reset(){
+        Random=Math.floor(Math.random()*101+19);
+        console.log(Random)
+        $('#number').text(Random);
+        GeneratedNumbers = [];
+        fourRandomNumbers();
+        total= 0;
+        $('#Combined').text(total);
+        }
+
+//adds the wins to the total
+function yay(){
+	alert("You won!!!");
+		wins++;
+		$('#numberWins').text(wins);
+		reset();
+}
+
+//adds the losses to the total
+function loser(){
+	alert ("You lose!!");
+		losses++;
+		$('#numberLosses').text(losses);
+		reset();
+}
+
+
+
+//sets up click for jewels
+		$('#one').on ('click', function(){
+			total = total + GeneratedNumbers[0];
+			console.log("New total= " + total);
+			$('#Combined').text(total);
+
+						//sets win/lose conditions
+					if (total == Random){
+						yay();
+					}
+					else if ( total > Random){
+						loser();
+					}
+		})
+
+		$('#two').on ('click', function(){
+			total = total + GeneratedNumbers[1];
+			console.log("New total= " + total);
+			$('#Combined').text(total);
+
+					if (total == Random){
+						yay();
+					}
+					else if ( total > Random){
+						loser();
+					}
+		})
+
+		$('#three').on ('click', function(){
+			total = total + GeneratedNumbers[2];
+			console.log("New total= " + total);
+			$('#Combined').text(total);
+
+	//sets win/lose conditions
+						if (total == Random){
+						yay();
+					}
+					else if ( total > Random){
+						loser();
+					}
+		})
+
+		$('#four').on ('click', function(){
+			total = total + GeneratedNumbers[3];
+			console.log("New total= " + total);
+			$('#Combined').text(total);
+
+
+						if (total == Random){
+						yay();
+					}
+					else if ( total > Random){
+						loser();
+					}
 		});
-
-		$('#gem2').on("click", function(){
-			click.play();
-			$('#gem2').animateCss('tada');
-			score+=gem2;
-			$('#score').html(score);
-			outcome();
-
-		});
-
-		$('#gem3').on("click", function(){
-			click.play();
-			$('#gem3').animateCss('tada');
-			score+=gem3;
-			$('#score').html(score);
-			outcome();
-			console.log("score: " +score);
-		});
-
-		$('#gem4').on("click", function(){
-			click.play();
-			$('#gem4').animateCss('tada');
-			score+=gem4;
-			$('#score').html(score);
-			outcome();
-			console.log("score: " +score);
-		});
-
-}; //playgame
-
-//Set-up and play the game=================================================
-setUp();
-playGame();
-console.log("Goal:" +goal);
-
-
 
 });
